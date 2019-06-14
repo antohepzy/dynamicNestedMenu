ab0c3af05fdea74e1af2c0c18f45f0397526b66a
antohepzy <47741661+antohepzy@users.noreply.github.com>
2019-06-14 23:01:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import {DropdownButton,Dropdown} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+	constructor(){
+    super();
+    this.state={}     
+    }
+
+    renderSubMenu(menuOptions){
+    	const subMenu = Object.values(menuOptions).map((option,i) => {
+
+    	
+    		
+    			return(
+    				
+    			<div>	
+    			
+       			{ option.children.length>0 
+
+    				?
+    			
+    			<DropdownButton id="dropdown-button-drop-down"  variant="transparent"  title={option.item}>     			
+    			{this.renderSubMenu(option.children)}</DropdownButton>
+   				
+    				:
+
+       			<Dropdown.Item eventKey={`item${i}`} style={{width:'505px'}}>{option.item}</Dropdown.Item>
+
+       			}
+       			<Dropdown.Divider /> 
+       			</div>);
+
+    			
+    		
+    	  
+    		
+    	});
+    	return(
+    	<div>{ subMenu }</div>);
+    
+    }
+
+ 
+ render(){
+
+ 	return (
+ 		
+ 		<div>
+ 		<Dropdown.Menu show className="App">{ this.renderSubMenu(data) }</Dropdown.Menu>
+ 		</div>
+ 		
+     	
+      );
+ }
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,45 @@
+ const data =  [ 
+	{ 
+	"item": "Home", 	
+	"children": [] 
+	}, 
+	{ 
+	"item": "Women", 
+	"children": [ 
+					{ 
+					"item": "Outwear",
+					 "children": [] 
+					}, 
+					{ 
+					"item": "Bottoms", 
+					"children": [] 
+					}, 
+					{ 
+					"item": "Tops", 
+					"children": [] 
+					}, 
+					{ 
+					"item": "Accessories", 
+					"children": []
+					}, 
+					{ 
+					"item": "Dresses", 
+					"children": [ 
+									{ 
+									"item": "Too short", 
+									"children": [] }, 
+									{ 
+									"item": "Too long", 
+									"children": [] 
+									},
+									{ 
+									"item": "Just right",
+									 "children": [] 
+									},
+				 				] 
+				 	} 
+				 ] 
+	} 
+]
+
+export default data;
